b375ff087d2409d5aae15d5afab55ab443a4ad99
Piotr Kowalski <piecioshka@gmail.com>
2015-09-02 21:19:38 +0200

    Display background on MenuState.

--- app/scripts/states/MenuState.es6.js
+++ app/scripts/states/MenuState.es6.js
@@ -1,10 +1,12 @@
 class MenuState extends Phaser.State {
     preload() {
         console.log('MenuState#preload');
+        this.load.image('bg-menu', './assets/graphics/backgrounds/bg-menu.jpg');
     }
 
     create() {
         console.log('MenuState#create');
+        this.add.image(0, 0, 'bg-menu');
     }
 
     update() {
