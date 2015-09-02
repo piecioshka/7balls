eaa2c7f267888498a9cba24abae449bec05508b8
Piotr Kowalski <piecioshka@gmail.com>
2015-09-02 21:48:11 +0200

    Use SearchingState. Redirect user to it after choose character. Setup common player object - cross states.

--- app/scripts/states/MenuState.es6.js
+++ app/scripts/states/MenuState.es6.js
@@ -3,31 +3,35 @@ import Vegeta from '../models/characters/Vegeta';
 
 class MenuState extends Phaser.State {
     preload() {
-        console.log('MenuState#preload');
         this.load.image('bg-menu', './assets/graphics/backgrounds/bg-menu.jpg');
         this.load.image('goku-card', './assets/graphics/characters/goku/goku-card.jpg');
         this.load.image('vegeta-card', './assets/graphics/characters/vegeta/vegeta-card.jpg');
     }
 
     create() {
-        console.log('MenuState#create');
         this.add.image(0, 0, 'bg-menu');
 
-        this.add.button(200, 130, 'goku-card', this._selectGoku);
-        this.add.button(400, 130, 'vegeta-card', this._selectVegeta);
+        this.add.button(200, 130, 'goku-card', this._selectGoku.bind(this));
+        this.add.button(400, 130, 'vegeta-card', this._selectVegeta.bind(this));
     }
 
     _selectGoku() {
-        this.player = new Goku({
+        // Add player object as common in all states.
+        this.game.player = new Goku({
             name: 'Son Gokū'
         });
+
+        // Move to next state: Searching
         this.state.start('Searching');
     }
 
     _selectVegeta() {
-        this.player = new Vegeta({
+        // Add player object as common in all states.
+        this.game.player = new Vegeta({
             name: 'Vegeta'
         });
+
+        // Move to next state: Searching
         this.state.start('Searching');
     }
 
--- app/scripts/states/SearchingState.es6.js
+++ app/scripts/states/SearchingState.es6.js
@@ -1,10 +1,11 @@
 class SearchingState extends Phaser.State {
     preload() {
-
+        this.load.image('bg-searching', './assets/graphics/backgrounds/bg-searching.jpg');
     }
 
     create() {
-
+        this.add.image(0, 0, 'bg-searching');
+        this.add.text(50, 50, `Hello ${this.game.player.name}`);
     }
 
     update() {
