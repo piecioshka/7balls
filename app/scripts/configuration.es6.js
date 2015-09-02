64a8ac6236c190d608f421a8e943e995dd676af8
Piotr Kowalski <piecioshka@gmail.com>
2015-09-02 21:19:18 +0200

    Create Configuration module.

---
+++ app/scripts/configuration.es6.js
@@ -0,0 +1,7 @@
+const Configuration = {
+    GAME_WIDTH: 800,
+    GAME_HEIGHT: 400,
+    GAME_RENDER_ID: 'game'
+};
+
+export default Configuration;
--- app/scripts/main.es6.js
+++ app/scripts/main.es6.js
@@ -1,8 +1,9 @@
+import Configuration from './configuration';
 import MenuState from './states/MenuState';
 
 class Game {
     constructor() {
-        this.game = new Phaser.Game(800, 200, Phaser.Canvas, 'game');
+        this.game = new Phaser.Game(Configuration.GAME_WIDTH, Configuration.GAME_HEIGHT, Phaser.Canvas, Configuration.GAME_RENDER_ID);
 
         this.game.state.add('Menu', MenuState);
         this.game.state.start('Menu');
