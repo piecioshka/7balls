99bfc1baf8b26571d3d6cccd40d481e35cc234a7
Piotr Kowalski <piecioshka@gmail.com>
2015-09-03 20:28:54 +0200

    Show welcome message on SearchingState.

--- app/scripts/states/SearchingState.es6.js
+++ app/scripts/states/SearchingState.es6.js
@@ -17,14 +17,26 @@ class SearchingState extends Phaser.State {
         let layer = map.createLayer('Tile Layer 1');
         layer.resizeWorld();
 
-        // this.add.image(0, 0, 'bg-searching');
-        this.add.text(50, 50, `Hello ${this.game.player.nickname} (${this.game.player.name})`);
-
-        if (this.game.player.exp.length) {
-            this.add.text(50, 100, `Your experience: ${this.game.player.exp.join('\n')}`);
-        }
+        this._showWelcomeMessage();
 
         this.game.player.sprite = this.add.sprite(10, 50, `${this.game.player.id}-searching`);
+    }
+
+    _showWelcomeMessage() {
+        this.welcomeMessage = this.add.text(this.game.width / 2, this.game.height / 2, `Hello ${this.game.player.nickname} (${this.game.player.name})`);
+        this.welcomeMessage.alpha = 0;
+        this.welcomeMessage.anchor.set(0.5, 0.5);
+        this.welcomeMessage.fontSize = 60;
+        this.welcomeMessage.fill = '#fff';
+        this.welcomeMessage.setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
+
+        this.add.tween(this.welcomeMessage).to({ alpha: 1 }, Phaser.Timer.SECOND / 2, Phaser.Easing.Linear.None, true);
+
+        this.time.events.add(Phaser.Timer.SECOND * 1.5, this._hideWelcomeMessage, this);
+    }
+
+    _hideWelcomeMessage() {
+        this.add.tween(this.welcomeMessage).to({ alpha: 0 }, Phaser.Timer.SECOND / 2, Phaser.Easing.Linear.None, true);
     }
 
     update() {
