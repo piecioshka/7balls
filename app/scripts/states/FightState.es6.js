799bc09fe7e7749ce2a1f71a75500323ae623452
Piotr Kowalski <piecioshka@gmail.com>
2015-09-03 23:08:11 +0200

    Add background to FightState.

--- app/scripts/states/FightState.es6.js
+++ app/scripts/states/FightState.es6.js
@@ -1,10 +1,10 @@
 class FightState extends Phaser.State {
     preload() {
-
+        this.load.image('bg-fight', './assets/graphics/backgrounds/bg-fight.jpg');
     }
 
     create() {
-
+        this.add.image(0, 0, 'bg-fight');
     }
 
     update() {
