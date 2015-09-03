81a25c6555d1bfa6e70d2d655ec8c96e7d93c1e0
Piotr Kowalski <piecioshka@gmail.com>
2015-09-03 19:53:23 +0200

    Create spritesheet and map (JSON format). Apply map to game.

--- app/scripts/states/SearchingState.es6.js
+++ app/scripts/states/SearchingState.es6.js
@@ -1,10 +1,18 @@
 class SearchingState extends Phaser.State {
     preload() {
-        this.load.image('bg-searching', './assets/graphics/backgrounds/bg-searching.jpg');
+        // this.load.image('bg-searching', './assets/graphics/backgrounds/bg-searching.jpg');
+        this.load.image('spr-searching', './assets/graphics/spritesheet/spr-searching.jpg');
+        this.load.tilemap('searching-1', './assets/maps/searching-1.json', null, Phaser.Tilemap.TILED_JSON);
     }
 
     create() {
-        this.add.image(0, 0, 'bg-searching');
+        let map = this.add.tilemap('searching-1');
+        map.addTilesetImage('spr-searching');
+
+        let layer = map.createLayer('Tile Layer 1');
+        layer.resizeWorld();
+
+        // this.add.image(0, 0, 'bg-searching');
         this.add.text(50, 50, `Hello ${this.game.player.nickname} (${this.game.player.name})`);
 
         if (this.game.player.exp.length) {
