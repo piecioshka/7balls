bf12d53d01ec0c89bfe809432b72a3eb1fa9cc29
Piotr Kowalski <piecioshka@gmail.com>
2015-09-05 22:27:51 +0200

    Add sound effect to ShenronState.

--- app/scripts/states/ShenronState.es6.js
+++ app/scripts/states/ShenronState.es6.js
@@ -1,12 +1,24 @@
 class ShenronState {
+    sound = {
+        ambienceThunder: null
+    };
+
     preload() {
         this.load.image('bg-shenron', './assets/graphics/backgrounds/bg-shenron.jpg');
+        this.load.audio('sound-ambience-thunder', './assets/sound/dbk/ambience_thunder.ogg');
     }
 
     create() {
         this.add.image(0, 0, 'bg-shenron');
+        this.sound.ambienceThunder = this.add.audio('sound-ambience-thunder');
 
-        this.time.events.add(Phaser.Timer.SECOND * 2, () => this.state.start('Fight'), this);
+        this.time.events.add(Phaser.Timer.SECOND * 2, this._setupFight, this);
+        this.sound.ambienceThunder.play();
+    }
+
+    _setupFight() {
+        this.state.start('Fight');
+        this.sound.ambienceThunder.stop();
     }
 
     update() {
