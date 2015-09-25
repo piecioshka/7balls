7ca174a0b8f3ffd1dec7bb3707f2e634e4c795ec
Piotr Kowalski <piecioshka@gmail.com>
2015-09-25 22:09:11 +0200

    Enable ESLint.

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,24 @@ module.exports = {
     resolve: {
         extensions: ['.es6.js', '.js', '']
     },
-
     entry: './app/scripts/main',
-
     output: {
         filename: 'bundle.js',
         path: './app/dist'
     },
-
     module: {
+        preLoaders: [
+            {
+                test: /\.es6\.js/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader'
+            }
+        ],
         loaders: [
             {
                 test: /\.es6\.js/,
-                loader: 'babel?stage=0',
-                exclude: /node_modules/
+                exclude: /node_modules/,
+                loader: 'babel-loader?stage=0'
             }
         ]
     }
