fcd0d8d8ba2a464211e4a816fe00f511fe499aa2
Piotr Kowalski <piecioshka@gmail.com>
2015-12-30 23:14:33 +0100

    Remove ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,15 @@ module.exports = {
     resolve: {
         extensions: ['.es6.js', '.js', '']
     },
+
     entry: './app/scripts/main',
+
     output: {
         filename: 'bundle.js',
         path: './app/dist'
     },
+
     module: {
-        preLoaders: [
-            {
-                test: /\.es6\.js/,
-                exclude: /node_modules/,
-                loader: 'eslint-loader?{globals:["Phaser"]}'
-            }
-        ],
         loaders: [
             {
                 test: /\.es6\.js/,
