207c12b663b8c54a95613351d43b9aff0f75243b
Piotr Kowalski <piecioshka@gmail.com>
2018-05-21 21:01:40 +0200

    Updates

---
+++ assets/locale/definition-types.js
@@ -0,0 +1,14 @@
+/**
+ * @typedef {Object} Locale
+ * @description JSON files in {@link `/app/locale/`} directory.
+ *
+ * @property {string} CONTROLS_INFO
+ * @property {string} COLLECT_DRAGON_BALLS
+ *
+ * @property {string} TIME
+ *
+ * @property {string} FIGHT_STATE_LEVEL_SHORT
+ * @property {string} FIGHT_STATE_WELCOME
+ * @property {string} FIGHT_STATE_PLAYER_DIED
+ * @property {string} FIGHT_STATE_PLAYER_WIN
+ */
